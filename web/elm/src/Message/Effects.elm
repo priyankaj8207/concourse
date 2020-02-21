9c3294377a365ebac0662626d9e277323fce0577
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-02-21 09:55:40 -0500

    web: add Storage module
    It's a bit cumbersome to require there to be 3 ports for every value we
    want to store in localStorage/sessionStorage (save, load, and receive).
    My goal with the Storage module is to make it easier to store more
    things in browser storage.
    
    Now, there are 3 ports for each of local and session storage: save,
    load, and receive. One downside to this approach is that the receive
    port will receive updates to all keys, rather than just the one the user
    wants to listen to. To "solve" this, I added a `Noop` Delivery in the
    case of the receipt of some other key.
    
    concourse/concourse#5193
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- web/elm/src/Message/Effects.elm
+++ web/elm/src/Message/Effects.elm
@@ -23,6 +23,15 @@ import Message.Message
         , VersionToggleAction(..)
         , VisibilityAction(..)
         )
+import Message.Storage
+    exposing
+        ( loadFromLocalStorage
+        , loadFromSessionStorage
+        , saveToLocalStorage
+        , saveToSessionStorage
+        , sideBarStateKey
+        , tokenKey
+        )
 import Network.Build
 import Network.BuildPlan
 import Network.BuildPrep
@@ -55,12 +64,6 @@ port tooltipHd : ( String, String ) -> Cmd msg
 port resetPipelineFocus : () -> Cmd msg
 
 
-port loadToken : () -> Cmd msg
-
-
-port saveToken : String -> Cmd msg
-
-
 port requestLoginRedirect : String -> Cmd msg
 
 
@@ -80,12 +83,6 @@ port rawHttpRequest : String -> Cmd msg
 
 
 port renderSvgIcon : String -> Cmd msg
-
-
-port loadSideBarState : () -> Cmd msg
-
-
-port saveSideBarState : Bool -> Cmd msg
 
 
 type alias StickyHeaderConfig =
@@ -161,8 +158,6 @@ type Effect
     | PinTeamNames StickyHeaderConfig
     | Scroll ScrollDirection String
     | SetFavIcon (Maybe BuildStatus)
-    | SaveToken String
-    | LoadToken
     | OpenBuildEventStream { url : String, eventTypes : List String }
     | CloseBuildEventStream
     | CheckIsVisible String
@@ -170,8 +165,10 @@ type Effect
     | Blur String
     | RenderSvgIcon String
     | ChangeVisibility VisibilityAction Concourse.PipelineIdentifier
-    | LoadSideBarState
+    | SaveToken String
+    | LoadToken
     | SaveSideBarState Bool
+    | LoadSideBarState
     | GetViewportOf DomID TooltipPolicy
     | GetElement DomID
 
@@ -416,12 +413,6 @@ runEffect effect key csrfToken =
         Scroll (ToId id) idOfThingToScroll ->
             scroll id idOfThingToScroll (.viewport >> .x) (.viewport >> .y)
 
-        SaveToken tokenValue ->
-            saveToken tokenValue
-
-        LoadToken ->
-            loadToken ()
-
         Focus id ->
             Browser.Dom.focus id
                 |> Task.attempt (always EmptyCallback)
@@ -450,11 +441,17 @@ runEffect effect key csrfToken =
                 csrfToken
                 |> Task.attempt (VisibilityChanged action pipelineId)
 
-        LoadSideBarState ->
-            loadSideBarState ()
+        SaveToken token ->
+            saveToLocalStorage ( tokenKey, Json.Encode.string token )
+
+        LoadToken ->
+            loadFromLocalStorage tokenKey
 
         SaveSideBarState isOpen ->
-            saveSideBarState isOpen
+            saveToSessionStorage ( sideBarStateKey, Json.Encode.bool isOpen )
+
+        LoadSideBarState ->
+            loadFromSessionStorage sideBarStateKey
 
         GetViewportOf domID tooltipPolicy ->
             Browser.Dom.getViewportOf (toHtmlID domID)
